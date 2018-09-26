e29a13b2f0bbce0717bd645f072d3c4e15645543
Danilo Horta <danilo.horta@gmail.com>
2018-09-26 04:27:34 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,6 @@
 git config --global user.email "danilo.horta@gmail.com"
 git config --global user.name "Danilo Horta"
 config="/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
-$config branch --set-upstream origin master
+$config branch --set-upstream-to origin master
 
 mkdir -p $HOME/.config/fish/functions
