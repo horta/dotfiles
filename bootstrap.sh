4a44071ab768e816a4f27014b4e083b7f73ecaa4
Danilo Horta <danilo.horta@gmail.com>
2018-09-26 04:16:13 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,6 @@
 
 git config --global user.email "danilo.horta@gmail.com"
 git config --global user.name "Danilo Horta"
+config push --set-upstream origin master
 
 mkdir -p $HOME/.config/fish/functions
