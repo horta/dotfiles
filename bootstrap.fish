97cdb26d34017dea3495e77af06b321bda6d8d3a
Danilo Horta <danilo.horta@gmail.com>
2018-09-26 05:59:59 +0100

    Update and rename bootstrap.sh to bootstrap.fish

--- bootstrap.fish
+++ bootstrap.fish
@@ -1,15 +1,18 @@
-#!/usr/bin/env bash
+#!/usr/bin/env fish
 
 git config --global user.email "danilo.horta@gmail.com"
 git config --global user.name "Danilo Horta"
-config="/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
-$config fetch --all
+set config "/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
+eval $config fetch --all
 git config --global push.default current
 git config --global credential.helper 'cache --timeout 3600000'
 
 mkdir -p $HOME/.config/fish/functions
-source ~/anaconda/etc/fish/conf.d/conda.fish
-conda activate base
+
+if type conda ^/dev/null
+    source ~/anaconda/etc/fish/conf.d/conda.fish
+    conda activate base
+end
 
 if [ $# -eq 0 ]
 then
