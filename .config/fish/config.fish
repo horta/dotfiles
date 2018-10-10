51cf5b315a5369317823cefd7647fd4da6892baa
Danilo Horta <danilo.horta@gmail.com>
2018-10-10 10:12:33 +0100

    config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,14 @@
+set PATH $HOME/.cargo/bin $PATH
+
+set GOPATH $HOME/go
+set PATH $PATH /usr/local/opt/go/libexec/bin
+set PATH $PATH $GOPATH/bin
+
+set PATH $PATH /usr/local/opt/llvm/bin
+
+set GOOGLE_APPLICATION_CREDENTIALS /Users/horta/.google-api-pypi-downloads.json
+
+if test -e /usr/local/anaconda3/etc/fish/conf.d/conda.fish
+    source /usr/local/anaconda3/etc/fish/conf.d/conda.fish
+    conda activate base
+end
