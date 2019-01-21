f49b863d60e6be0e2973a85c5eb58afaca1f63e9
Danilo Horta <danilo.horta@gmail.com>
2019-01-21 12:52:30 +0000

    ok

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
 set -l PATH_BAK $PATH[-1..1]
 for p in $PATH_BAK
-   if string match -q -r "/usr/local/anaconda.*" $p
+   if string match -q -r "naconda.*" $p
        if set -l index (contains -i $p $PATH)
            set -e PATH[$index]
        end
@@ -12,5 +12,13 @@ if status is-interactive
 and set -q TMUX
 and not set -q CONDA_EXE
     source /usr/local/anaconda3/etc/fish/conf.d/conda.fish
+    conda activate
+end
+
+if status is-interactive
+and set -q YODA
+    set -gx CONDA_ENVS_PATH /nfs/software/stegle/users/horta/conda-envs
+    #source /usr/local/anaconda3/etc/fish/conf.d/conda.fish
+    source /nfs/software/stegle/system/Anaconda3-2018.12-Linux-x86_64/etc/fish/conf.d/conda.fish
     conda activate
 end
