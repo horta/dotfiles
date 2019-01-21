4169287652f020355365f3467ecf42b020b53690
Danilo Horta <danilo.horta@gmail.com>
2019-01-21 23:58:45 +0000

    ok

--- .bash_profile
+++ .bash_profile
@@ -5,3 +5,19 @@ function config()
 {
     /usr/bin/env git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
+
+function vim()
+{
+    _vim=$(which vim)
+    if ! $?;
+    then
+        return 1
+    fi
+    _nvim=$(which nvim)
+    if $?;
+    then
+        _nvim $@
+    else
+        _vim $@
+    fi
+}
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,27 +1,27 @@
-set -l PATH_BAK $PATH[-1..1]
-for p in $PATH_BAK
-   if string match -q -r "naconda.*" $p
-       if set -l index (contains -i $p $PATH)
-           set -e PATH[$index]
-       end
-       set PATH $p $PATH
-   end
-end
+#set -l PATH_BAK $PATH[-1..1]
+#for p in $PATH_BAK
+#   if string match -q -r "naconda.*" $p
+#       if set -l index (contains -i $p $PATH)
+#           set -e PATH[$index]
+#       end
+#       set PATH $p $PATH
+#   end
+#end
 
-if status is-interactive
-and set -q TMUX
-and not set -q CONDA_EXE
-    source /usr/local/anaconda3/etc/fish/conf.d/conda.fish
-    conda activate
-end
+#if status is-interactive
+#and set -q TMUX
+#and not set -q CONDA_EXE
+#    source /usr/local/anaconda3/etc/fish/conf.d/conda.fish
+#    conda activate
+#end
 
-if status is-interactive
-and set -q CLUSTER_NAME
-and [ $CLUSTER_NAME = "YODA" ]
-    set -gx CONDA_ENVS_PATH /nfs/software/stegle/users/horta/conda-envs
-    #source /usr/local/anaconda3/etc/fish/conf.d/conda.fish
-    source /nfs/software/stegle/system/Anaconda3-2018.12-Linux-x86_64/etc/fish/conf.d/conda.fish
-    conda activate
-end
+#if status is-interactive
+#and set -q CLUSTER_NAME
+#and [ $CLUSTER_NAME = "YODA" ]
+#    set -gx CONDA_ENVS_PATH /nfs/software/stegle/users/horta/conda-envs
+#    #source /usr/local/anaconda3/etc/fish/conf.d/conda.fish
+#    source /nfs/software/stegle/system/Anaconda3-2018.12-Linux-x86_64/etc/fish/conf.d/conda.fish
+#    conda activate
+#end
 
 set -gx SHELL fish
