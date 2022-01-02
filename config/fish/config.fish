a796bfa73303e6403e861118ee4178c640920b48
Danilo Horta <danilo.horta@pm.me>
2022-01-02 14:34:08 +0100

    not working commands

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,12 +3,12 @@ if status is-interactive
     source ~/.alias
 
     if [ "$OS" = "Darwin" ]
-        source ~/.alias_macos                                                                                                  
-    end
-    
-    if [ "$OS" = "Linux" ]
-        source ~/.alias_linux                                                                                                  
-    end                                                                                                                       
+    #     source ~/.alias_macos                                                                                                  
+    # end
+    # 
+    # if [ "$OS" = "Linux" ]
+    #     source ~/.alias_linux                                                                                                  
+    # end                                                                                                                       
     mcfly init fish | source                                                                                                   
 end                                                                                                                            
                                                                                                                                
