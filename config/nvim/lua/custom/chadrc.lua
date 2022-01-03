7831681d154fc1560fb3c8d5dbc32a9af0b1ab6d
Danilo Horta <danilo.horta@pm.me>
2022-01-03 20:41:58 +0100

    git open

--- config/nvim/lua/custom/chadrc.lua
+++ config/nvim/lua/custom/chadrc.lua
@@ -1,32 +1,26 @@
--- This is an example chadrc file , its supposed to be placed in /lua/custom dir
--- lua/custom/chadrc.lua
-
 local M = {}
-
--- make sure you maintain the structure of `core/default_config.lua` here,
--- example of changing theme:
 
 M.options = {}
 
+M.ui = {
+    theme = "gruvchad",
+}
+
 M.plugins = {
-   status = { nvimtree = false },
+    status = { nvimtree = false, blankline = false },
 }
 
 M.mappings = {}
 
 M.mappings.plugins = {
-   nvimtree = {
-      toggle = {},
-      focus = {},
-   },
-   bufferline = {
-      next_buffer = "<c-n>",
-      prev_buffer = "<c-p>",
-   },
-}
-
-M.ui = {
-   theme = "gruvchad",
+    nvimtree = {
+        toggle = {},
+        focus = {},
+    },
+    bufferline = {
+        next_buffer = "<c-n>",
+        prev_buffer = "<c-p>",
+    },
 }
 
 return M
